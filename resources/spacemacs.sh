12c0b716f610ef1966074ff49488988383cef9b3
Kevin Feng <49735863+HereIsKevin@users.noreply.github.com>
2019-09-22 17:29:17 -0700

    Fix initialization bug in resources/spacemacs.sh

--- resources/spacemacs.sh
+++ resources/spacemacs.sh
@@ -1,24 +1,18 @@
 #!/bin/bash
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do
-    DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 if [ -e "${HOME}/.spacemacs.d/" ]; then
     if [ ! -e "${HOME}/.spacemacs.d/.emacs.d/" ]; then
-        cp "${DIR}/../Resources/.spacemacs.d/.emacs.d/" "${HOME}/.spacemacs.d/.emacs.d/"
+        cp -r "${DIR}/../Resources/.spacemacs.d/.emacs.d/" "${HOME}/.spacemacs.d/.emacs.d/"
         HOME="${HOME}/.spacemacs.d"
         "${DIR}/Emacs.app/Contents/MacOS/emacs"
-    elif [ -e "${HOME}/.spacemacs.d/.emacs.d/" ]; then
-        HOME="${HOME}/.spacemacs.d"
+    else
+        HOME="${HOME}/.spacemacs.d/"
         "${DIR}/Emacs.app/Contents/MacOS/emacs"
     fi
-elif [ ! -e "${HOME}/.spacemacs.d" ]; then
-    cp "${DIR}/../Resources/.spacemacs.d/" "${HOME}/.spacemacs.d/"
+else
+    cp -r "${DIR}/../Resources/.spacemacs.d/" "${HOME}/.spacemacs.d/"
     HOME="${HOME}/.spacemacs.d"
     "${DIR}/Emacs.app/Contents/MacOS/emacs"
 fi
